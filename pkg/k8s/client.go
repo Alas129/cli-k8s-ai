909694a7a2fc843a21b412795f2cfdcbae57121c
alas129 <alas129@163.com>
2024-08-04 22:34:08 -0700

    Added more function to interact with K8S

--- pkg/k8s/client.go
+++ pkg/k8s/client.go
@@ -3,6 +3,7 @@ package k8s
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -29,4 +30,20 @@ func NewClient(kubeconfigPath string) (*Client, error) {
 
 func (c *Client) GetPods(namespace string) (*corev1.PodList, error) {
 	return c.clientset.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
+}
+
+func (c *Client) GetServices(namespace string) (*corev1.ServiceList, error) {
+	return c.clientset.CoreV1().Services(namespace).List(context.Background(), metav1.ListOptions{})
+}
+
+func (c *Client) GetDeployments(namespace string) (*appsv1.DeploymentList, error) {
+	return c.clientset.AppsV1().Deployments(namespace).List(context.Background(), metav1.ListOptions{})
+}
+
+func (c *Client) GetNodes() (*corev1.NodeList, error) {
+	return c.clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+}
+
+func (c *Client) GetNamespaces() (*corev1.NamespaceList, error) {
+	return c.clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
 }
--- pkg/openai/client.go
+++ pkg/openai/client.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	
+
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+
 	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/Alas129/cli-k8s-ai/pkg/k8s"
 	"github.com/sashabaranov/go-openai"
@@ -54,6 +56,44 @@ func (c *Client) ProcessUserInput(input string, k8sClient *k8s.Client) (string, 
 				"required": []string{"namespace"},
 			},
 		},
+		{
+			Name: "get_services",
+			Parameters: map[string]interface{}{
+				"type": "object",
+				"properties": map[string]interface{}{
+					"namespace": map[string]interface{}{
+						"type":        "string",
+						"description": "The namespace to list services from",
+					},
+				},
+				"required": []string{"namespace"},
+			},
+		},
+		{
+			Name: "get_deployments",
+			Parameters: map[string]interface{}{
+				"type": "object",
+				"properties": map[string]interface{}{
+					"namespace": map[string]interface{}{
+						"type":        "string",
+						"description": "The namespace to list deployments from",
+					},
+				},
+				"required": []string{"namespace"},
+			},
+		},
+		{
+			Name: "get_nodes",
+			Parameters: map[string]interface{}{
+				"type": "object",
+			},
+		},
+		{
+			Name: "get_namespaces",
+			Parameters: map[string]interface{}{
+				"type": "object",
+			},
+		},
 	}
 
 	resp, err := c.client.CreateChatCompletion(
@@ -84,6 +124,42 @@ func (c *Client) ProcessUserInput(input string, k8sClient *k8s.Client) (string, 
 				return "", fmt.Errorf("error getting pods: %v", err)
 			}
 			return formatPodList(pods), nil
+		case "get_services":
+			var args struct {
+				Namespace string `json:"namespace"`
+			}
+			if err := json.Unmarshal([]byte(resp.Choices[0].Message.FunctionCall.Arguments), &args); err != nil {
+				return "", fmt.Errorf("error unmarshaling function arguments: %v", err)
+			}
+			services, err := k8sClient.GetServices(args.Namespace)
+			if err != nil {
+				return "", fmt.Errorf("error getting services: %v", err)
+			}
+			return formatServiceList(services), nil
+		case "get_deployments":
+			var args struct {
+				Namespace string `json:"namespace"`
+			}
+			if err := json.Unmarshal([]byte(resp.Choices[0].Message.FunctionCall.Arguments), &args); err != nil {
+				return "", fmt.Errorf("error unmarshaling function arguments: %v", err)
+			}
+			deployments, err := k8sClient.GetDeployments(args.Namespace)
+			if err != nil {
+				return "", fmt.Errorf("error getting deployments: %v", err)
+			}
+			return formatDeploymentList(deployments), nil
+		case "get_nodes":
+			nodes, err := k8sClient.GetNodes()
+			if err != nil {
+				return "", fmt.Errorf("error getting nodes: %v", err)
+			}
+			return formatNodeList(nodes), nil
+		case "get_namespaces":
+			namespaces, err := k8sClient.GetNamespaces()
+			if err != nil {
+				return "", fmt.Errorf("error getting namespaces: %v", err)
+			}
+			return formatNamespaceList(namespaces), nil
 		}
 	}
 
@@ -94,6 +170,38 @@ func formatPodList(pods *corev1.PodList) string {
 	result := "Pods:\n"
 	for _, pod := range pods.Items {
 		result += fmt.Sprintf("- %s (Status: %s)\n", pod.Name, pod.Status.Phase)
+	}
+	return result
+}
+
+func formatServiceList(services *corev1.ServiceList) string {
+	result := "Services:\n"
+	for _, service := range services.Items {
+		result += fmt.Sprintf("- %s (Type: %s)\n", service.Name, service.Spec.Type)
+	}
+	return result
+}
+
+func formatDeploymentList(deployments *appsv1.DeploymentList) string {
+	result := "Deployments:\n"
+	for _, deployment := range deployments.Items {
+		result += fmt.Sprintf("- %s (Replicas: %d)\n", deployment.Name, *deployment.Spec.Replicas)
+	}
+	return result
+}
+
+func formatNodeList(nodes *corev1.NodeList) string {
+	result := "Nodes:\n"
+	for _, node := range nodes.Items {
+		result += fmt.Sprintf("- %s (Status: %s)\n", node.Name, node.Status.Phase)
+	}
+	return result
+}
+
+func formatNamespaceList(namespaces *corev1.NamespaceList) string {
+	result := "Namespaces:\n"
+	for _, namespace := range namespaces.Items {
+		result += fmt.Sprintf("- %s\n", namespace.Name)
 	}
 	return result
 }
